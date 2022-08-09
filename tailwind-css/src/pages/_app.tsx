c82a2513363cb4a045ca78d62373ab17a67fcc78
louis.luo <louis0420tw@gmail.com>
2022-08-10 02:17:08 +0800

    feat(root): add tailwind config

--- tailwind-css/src/pages/_app.tsx
+++ tailwind-css/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import "../styles/global.css"
 
 const NextApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
